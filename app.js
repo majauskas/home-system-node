2fc5733123b18e1ff20ad83eba791d4ccc726480
Minde <m.ajauskas@hotmail.it>
2015-03-09 23:48:05 +0100

    update

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ var server = app.listen(process.env.PORT || 8081, function () {
 
   var port = server.address().port;
   console.log('app listening at http://%s:%s', host, port);
+  email("Home System Attivato", "App listening at http://"+host+":"+port);
 });
 
 app.get('/home-system', function(req, res) {
