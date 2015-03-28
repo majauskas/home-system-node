d36db58c549d16aa62f13264774a1662e614a1b3
Minde <m.ajauskas@hotmail.it>
2015-03-28 16:34:07 +0100

    aaaa

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ var server = app.listen(process.env.PORT || 8081, function () {
   console.log('app listening at http://%s:%s', host, port);
   MCP23017.scan(function(data) {
 	  console.log("MCP23017: ", data);
-	  email("MCP23017 Attivato", data);
+	  email("MCP23017 Attivato", data.bin);
   });
 
   
