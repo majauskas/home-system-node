30ba6c3bdb6be1c38b9982387e764ad60387f548
Administrator <Administrator@10.221.6.69>
2015-05-20 11:46:36 +0200

    update

--- app.js
+++ app.js
@@ -737,51 +737,51 @@ setInterval(function() {
         			}
         		});
         		
-//        		setTimeout(function() {
+        		setTimeout(function() {
+
+    				LAN_DEVICE.find({}).exec(function(err, data) {
+    					
+    					var macs = "";
+    					entries.forEach(function(target) {
+    						macs += target.split(' ')[0]; 
+    					});
+        				
+    					if(data && data.length > 0){
+    						data.forEach(function(device) {
+    							if(macs.indexOf(device.mac) >= 0){
+    								obj.exists = true;
+    								obj.lastLogin = new Date();
+    							}	
+    							LAN_DEVICE.findOneAndUpdate({mac : device.mac}, obj, function (err, doc) {});	
+//    							if(!device.manufacturer){
+//    								var macaddress = device.mac;
+//    								request({'url':'http://www.admin-toolkit.com/php/mac-lookup-vendor.php?maclist='+macaddress}, function (error, response, body) {
+//    								    if (!error && response.statusCode == 200) {
+//    								        console.log("body: ", body);
+//    								        console.log("response: ", response);
+////    								        var arr = body.split('|');
+////    								        if(arr.length>0){
+////        								        var manufacturer = arr[1];
+////        								        console.log("manufacturer: ", manufacturer);
+////        								        console.log("manufacturer: ", manufacturer.trim());
+////        								        LAN_DEVICE.findOneAndUpdate({mac : macaddress}, {manufacturer:manufacturer.trim()}, function (err, doc) {});
+////    								        }
 //
-//    				LAN_DEVICE.find({}).exec(function(err, data) {
-//    					
-//    					var macs = "";
-//    					entries.forEach(function(target) {
-//    						macs += target.split(' ')[0]; 
-//    					});
-//        				
-//    					if(data && data.length > 0){
-//    						data.forEach(function(device) {
-//    							if(macs.indexOf(device.mac) >= 0){
-//    								obj.exists = true;
-//    								obj.lastLogin = new Date();
-//    							}	
-//    							LAN_DEVICE.findOneAndUpdate({mac : device.mac}, obj, function (err, doc) {});	
-////    							if(!device.manufacturer){
-////    								var macaddress = device.mac;
-////    								request({'url':'http://www.admin-toolkit.com/php/mac-lookup-vendor.php?maclist='+macaddress}, function (error, response, body) {
-////    								    if (!error && response.statusCode == 200) {
-////    								        console.log("body: ", body);
-////    								        console.log("response: ", response);
-//////    								        var arr = body.split('|');
-//////    								        if(arr.length>0){
-//////        								        var manufacturer = arr[1];
-//////        								        console.log("manufacturer: ", manufacturer);
-//////        								        console.log("manufacturer: ", manufacturer.trim());
-//////        								        LAN_DEVICE.findOneAndUpdate({mac : macaddress}, {manufacturer:manufacturer.trim()}, function (err, doc) {});
-//////    								        }
-////
-////    								    }
-////    							    });    								
-////    							}
-//    							
-//    							
-//    							
-//    						});	
-//    						
-//    						
-//    
-//    					}
-//					
-//				});	         			
-//        			
-//        		}, 100);
+//    								    }
+//    							    });    								
+//    							}
+    							
+    							
+    							
+    						});	
+    						
+    						
+    
+    					}
+					
+				});	         			
+        			
+        		}, 1000);
         		
         		
         		
