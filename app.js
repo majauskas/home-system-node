a30b84418d8c6dcb696ee43fdc6b326714c083cb
minde-ags <m.ajauskas@hotmail.it>
2016-04-27 14:20:14 +0200

    u

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var request = require('request');
 var child_process = require('child_process');
 var SunCalc = require('suncalc');
 var exec = child_process.exec;
-//var BASKETUISP = require('./lib/BASKETUISP.js');
+var BASKETUISP = require('./lib/BASKETUISP.js');
 
 //var arp = null; try { arp = require('arp-a'); } catch (e) {}
 
@@ -1227,12 +1227,12 @@ schedulers.execute(database, LightsController);
 //var wa = new waApi(393473834506, password, { displayName: 'Minde', debug: true });
 //wa.sendMessageWithBody({ content: "Message Content", to: "00393473834506"});
 
-//var latestNewsletter=null;
-//BASKETUISP.readContent(function(data) {
-//	
-//	if(latestNewsletter!==null && latestNewsletter !== data.newsletter){
-//		console.log("send email");
-//		 email("UISP newsletter", data.newsletter);
-//	}
-//	latestNewsletter = data.newsletter;
-//});
+var latestNewsletter=null;
+BASKETUISP.readContent(function(data) {
+	console.log("BASKETUISP",new Date(),data.newsletter);
+	if(latestNewsletter!==null && latestNewsletter !== data.newsletter){
+		console.log("send email",data.newsletter);
+		 email("UISP newsletter", data.newsletter);
+	}
+	latestNewsletter = data.newsletter;
+});
